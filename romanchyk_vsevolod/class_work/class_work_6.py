66e9b405693575c941ec71ed0c883e017d2e9fd8
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2026-05-26 21:42:52 +0300

    мой шестой commit

---
+++ romanchyk_vsevolod/class_work/class_work_6.py
@@ -0,0 +1,48 @@
+# import time
+# from lib2to3.pgen2 import driver
+#
+# from selenium import webdriver
+# from selenium.webdriver.common import actions
+# from selenium.webdriver.common.by import By
+#
+#
+# def test_buttons():
+#
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#
+#     driver.get('https://demoqa.com/buttons')
+#
+#     time.sleep(2)
+#     actions.double_click(By.ID, 'doubleClickBtn').perform()
+#     assert driver.find_element(By.ID, 'doubleClickMessage'), 'Сообщение с дабл кликом'
+#
+#     time.sleep(2)
+#     actions.context_click(By.ID, 'rightClickBtn').perform()
+#     assert driver.find_element(By.ID, 'rightClickMessage'), 'Сообщение с правым кликом'
+#
+#
+#     time.sleep(10)
+#     driver.find_element(By.ID, 'IOP4h').click()
+
+
+import time
+from lib2to3.pgen2 import driver
+
+from selenium import webdriver
+from selenium.webdriver.common import actions
+from selenium.webdriver.common.by import By
+
+
+def test_buttons():
+
+    driver = webdriver.Chrome()
+
+    driver.maximize_window()
+    driver.get('https://demoqa.com/links')
+
+    time.sleep(2)
+    driver.find_element(By.ID, 'simpleLink').click()
+    driver.switch_to.window(driver.window_handles[1])
+    driver.quit()
+
