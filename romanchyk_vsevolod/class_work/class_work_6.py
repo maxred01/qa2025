6343d7f909bde1b19320be1aca264fc7bde8af21
AlexeyZabrodsky <LeshaZabrodsky@gmail.com>
2026-05-28 21:51:28 +0300

    мой шестой commit

--- romanchyk_vsevolod/class_work/class_work_6.py
+++ romanchyk_vsevolod/class_work/class_work_6.py
@@ -27,22 +27,20 @@
 
 
 import time
+
 from lib2to3.pgen2 import driver
 
 from selenium import webdriver
 from selenium.webdriver.common import actions
 from selenium.webdriver.common.by import By
+from romanchyk_vsevolod.conftest import driver
 
 
 def test_buttons():
 
-    driver = webdriver.Chrome()
-
-    driver.maximize_window()
     driver.get('https://demoqa.com/links')
 
-    time.sleep(2)
     driver.find_element(By.ID, 'simpleLink').click()
     driver.switch_to.window(driver.window_handles[1])
-    driver.quit()
+
 
